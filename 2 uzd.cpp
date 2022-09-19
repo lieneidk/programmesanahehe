e9cb6c57cec1f3eea590e4d1274e1ca173f28486
lieneidk <112851699+lieneidk@users.noreply.github.com>
2022-09-19 10:01:17 +0300

    Add files via upload

---
+++ 2 uzd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using nmespace std;
+
+int main () {
+
+    int x,y;
+    string z;
+
+cout<<"ievadi vienu no dabîbâm +,-, /, *"<<endl;
+cin>>z;
+
+if (z == "+"){
+    cout<<" ievadi divus skaitïus"<<endl;
+    cin>> x>>y;
+    cout<<"atbilde ir "<<x + y;
+
+}
+}
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int a;
+    int b;
+
+    cout<<"ievadi skaitli"<<endl;
+    cin>>a;
+    cout<<"ievadi vçl vienu skaitli"<<endl;
+    cin>>b;
+
+
+ if (a>b) {
+    cout<<"skaitlis "<<a<<" lielâks nekâ skaitlis "<<b<<""<<endl;
+ } else if (a<b) {
+    cout<<"skaitlis "<<b<<" lielâks nekâ skaitlis "<<a<<""<<endl;
+ } else {
+    cout<<"skaitïi "<<a<<" un "<<b<<" ir vienâdi"<<endl;
+ }}
