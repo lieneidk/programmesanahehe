acb89ec8cd92e9cbf53340a4e56d0fc4970af585
lieneidk <112851699+lieneidk@users.noreply.github.com>
2022-11-02 10:44:09 +0200

    Add files via upload

---
+++ B uzd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+int n;
+int a, b, c;
+
+
+cout<<"ievadi naturalu skaitli"<<endl;
+cin>>n;
+
+while (a^2+b^2=c^2){
+    if(a <= b <= c <= n){
+        cout<<"skaitli atbilst nosacijumiem"<<endl;
+    }else{
+        cout<<"skaitli neatbilst nosacijumiem"<<endl;
+    }
+}
+
+
+}
