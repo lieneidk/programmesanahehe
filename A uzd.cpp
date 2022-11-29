438c3473f8625a07de361c6fa46527f420d5cf4c
lieneidk <112851699+lieneidk@users.noreply.github.com>
2022-11-29 21:43:58 +0200

    Update A uzd.cpp

--- A uzd.cpp
+++ A uzd.cpp
@@ -7,27 +7,28 @@ using namespace std;
 
 int main (){
 
-
+//nosaka integers un stringu, kas noteiks programmas atkartosanu
     int a;
     int b;
     int c;
     int yn = 0;
     string k;
 
-
+//nosakam, ka while cikls atkartojas, kamer integer yn ir mazaks par vai vienads ar 0
     while(yn <= 0){
 
         yn = yn -1;
 
         cout<<"ievadi divus naturalus skaitlus"<<endl;
         cin>>a>>b;
-
+//ar for nosaka, ka funkcija iziet cauri visiem skaitliem sakot no 1, kas mazaki par a un b 
     for (c=1; c<= b && c<= a; c++){
+//ar if nosaka, ka jaizprinte ta c varetiba, kas bez atbikuma dalas gan ar a, gan b
         if(a%c==0 && b%c==0){
             cout<<c<<endl;
         }
     }
-
+//dodam lietotajam iespeju izveleties turpinat vai neturpinat programmu ievadot y vai n. (atsauce uz ieprieksejo while ciklu)
     while (yn < 0){
 
         cout<<"vai velies turpinat?"<<endl;
